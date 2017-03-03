055b2c8b1cd1d33e419f26df879b22eab5d45e58
Jesse Rosenberger <git@jro.cc>
2017-03-03 03:03:30 +0200

    22

--- packages/test-in-console/run.sh
+++ packages/test-in-console/run.sh
@@ -7,21 +7,24 @@ export METEOR_HOME=`pwd`
 # Clear dev_bundle/.npm to ensure consistent test runs.
 ./meteor npm cache clear
 
-# Just in case these packages haven't been installed elsewhere.
-./meteor npm install -g selenium-webdriver@3.0.0-beta-2
-
 export PATH=$METEOR_HOME:$PATH
 # synchronously get the dev bundle and NPM modules if they're not there.
 ./meteor --get-ready || exit 1
 
 export URL='http://localhost:4096/'
 
+node --version
+
+tmp_npm_path=".tmp-test-npms/node_modules"
+mkdir -p ${tmp_npm_path}
+npm install -g selenium-webdriver@3.0.0-beta-2 --prefix ${tmp_npm_path}
+
 exec 3< <(meteor test-packages --driver-package test-in-console -p 4096 --exclude ${TEST_PACKAGES_EXCLUDE:-''})
 EXEC_PID=$!
 
 sed '/test-in-console listening$/q' <&3
-export NODE_PATH="${METEOR_HOME}/dev_bundle/lib/node_modules"
-script ./meteor node "$METEOR_HOME/packages/test-in-console/runner.js"
+export NODE_PATH="${METEOR_HOME}/${tmp_npm_path}"
+script node "$METEOR_HOME/packages/test-in-console/runner.js"
 STATUS=$?
 
 pkill -TERM -P $EXEC_PID
