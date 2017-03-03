bc5e50808b8ba2df30103a9eb0c0122e1fc394aa
Jesse Rosenberger <git@jro.cc>
2017-03-03 18:37:23 +0200

    Test

--- packages/test-in-console/run.sh
+++ packages/test-in-console/run.sh
@@ -13,20 +13,19 @@ export PATH=$METEOR_HOME:$PATH
 
 export URL='http://localhost:4096/'
 
-node --version
+./meteor node --version
 
 tmp_npm_path=".tmp-test-npms/node_modules"
 mkdir -p ${tmp_npm_path}
-npm install npm@4 --global
-npm init -y --prefix ${tmp_npm_path}
-npm install selenium-webdriver@3.0.0 --save --prefix ${tmp_npm_path}
+./meteor npm init -y --prefix ${tmp_npm_path}
+./meteor npm install selenium-webdriver@3.0.0-beta-3 --save --prefix ${tmp_npm_path}
 
 exec 3< <(meteor test-packages --driver-package test-in-console -p 4096 --exclude ${TEST_PACKAGES_EXCLUDE:-''})
 EXEC_PID=$!
 
 sed '/test-in-console listening$/q' <&3
 export NODE_PATH="${METEOR_HOME}/${tmp_npm_path}"
-node "$METEOR_HOME/packages/test-in-console/runner.js" | tee
+./meteor node "$METEOR_HOME/packages/test-in-console/runner.js" 2>&1
 STATUS=$?
 
 pkill -TERM -P $EXEC_PID
