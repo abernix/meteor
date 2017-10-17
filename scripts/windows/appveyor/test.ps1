fabb45f97db53dc5668d1e26878b0268e1d81a20
Jesse Rosenberger <git@jro.cc>
2017-10-17 13:52:09 +0300

    script

---
+++ scripts/windows/appveyor/test.ps1
@@ -0,0 +1,21 @@
+﻿$jUnit = Join-Path $env:TEMP 'self-test-junit-0.xml'
+
+$testList = @(
+  'modules - test app'
+  'autoupdate'
+  'create$'
+  # 'dynamic import'
+  'colons'
+)
+
+$tests = $testList -Join '|'
+
+& cmd.exe /C .\meteor.bat self-test --junit "$jUnit" "$tests" --exclude "$env:SELF_TEST_EXCLUDE" '2>&1'
+
+$selfTestExitCode = $LASTEXITCODE
+
+Write-Host "Self Test Exit $selfTestExitCode"
+
+If ($selfTestExitCode -ne 0) {
+  Exit $selfTestExitCode
+}
