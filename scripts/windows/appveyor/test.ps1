bca0d2314a5168789ace5efcbea7de504b397cea
Jesse Rosenberger <git@jro.cc>
2017-10-17 13:54:41 +0300

    typo

--- scripts/windows/appveyor/test.ps1
+++ scripts/windows/appveyor/test.ps1
@@ -1,20 +1,25 @@
-﻿$jUnit = Join-Path $env:TEMP 'self-test-junit-0.xml'
+﻿# For now, we only have one script.
+$jUnit = Join-Path $env:TEMP 'self-test-junit-0.xml'
 
-$testList = @(
+$tests = @(
   'modules - test app'
+  '^assets'
   'autoupdate'
   'create$'
   # 'dynamic import'
   'colons'
-)
-
-$tests = $testList -Join '|'
+) -Join '|'
 
 & cmd.exe /C .\meteor.bat self-test --junit "$jUnit" "$tests" --exclude "$env:SELF_TEST_EXCLUDE" '2>&1'
-
 $selfTestExitCode = $LASTEXITCODE
 
-Write-Host "Self Test Exit $selfTestExitCode"
+Write-Host "Self Test Exit $selfTestExitCode" -ForegroundColor Green
+
+Write-Host "Uploading JUnit test results..." -ForegroundColor Magenta
+$wc = New-Object 'System.Net.WebClient'
+Get-ChildItem $env:TEMP 'self-test-junit-*.xml' | Foreach-Object {
+  $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", ($_.FullName))
+}
 
 If ($selfTestExitCode -ne 0) {
   Exit $selfTestExitCode
